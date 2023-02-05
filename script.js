18a64a52f83d5646f759923592da7ab0e764d602
Abdullah Ramadan <74230257+Abdullahali310@users.noreply.github.com>
2023-02-05 14:13:14 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function calc(result){
+    form.res.value=form.res.value+result;
+  }
+
+  function backspace() {
+    document.form.res.value = document.form.res.value.substring(0, document.form.res.value.length - 1);
+  }
+  
